e3ab2961f6cab4233a7e548f9fece367834d731a
leozhou <492753662@qq.com>
2017-11-20 15:02:45 +0800

    add convet to markdown

---
+++ src/test/java/com/le0zh0u/Swagger2MarkupTest.java
@@ -0,0 +1,66 @@
+package com.le0zh0u;
+
+import com.le0zh0u.config.Swagger2Config;
+import io.github.swagger2markup.GroupBy;
+import io.github.swagger2markup.Language;
+import io.github.swagger2markup.Swagger2MarkupConfig;
+import io.github.swagger2markup.Swagger2MarkupConverter;
+import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+import io.github.swagger2markup.markup.builder.MarkupLanguage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by zhouchunjie on 2017/11/16.
+ */
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@AutoConfigureRestDocs(outputDir = "build/asciidoc/snippets")
+@SpringBootTest(classes = {SwaggerSpringBootSampleApplication.class, Swagger2Config.class})
+@AutoConfigureMockMvc
+public class Swagger2MarkupTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Swagger2MarkupTest.class);
+
+    @Autowired private MockMvc mockMvc;
+
+    @Test
+    public void createSpringfoxSwaggerJson() throws Exception {
+        //String designFirstSwaggerLocation = Swagger2MarkupTest.class.getResource("/swagger.yaml").getPath();
+
+        String outputDir = System.getProperty("io.springfox.staticdocs.outputDir");
+        MvcResult mvcResult =
+            this.mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andReturn();
+
+        MockHttpServletResponse response = mvcResult.getResponse();
+        String swaggerJson = response.getContentAsString();
+        Files.createDirectories(Paths.get(outputDir));
+        try (BufferedWriter writer = Files
+            .newBufferedWriter(Paths.get(outputDir, "swagger.json"), StandardCharsets.UTF_8)) {
+            writer.write(swaggerJson);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
